a27d8c232b3f2ced89caeb586a15cf4956731d62
Akash Verma <akashverma75890@gmail.com>
2025-03-23 13:33:31 +0530

    update show commands

--- packages/laravelgithubapis/src/Commands/ShowGitHelpCommand.php
+++ packages/laravelgithubapis/src/Commands/ShowGitHelpCommand.php
@@ -47,8 +47,10 @@ Saved GitHub Commands:
 10. Create a Pull Request:
     php artisan github:create-pull-request
 
-11. Show git help:
+11. command to Show git help:
     php artisan show-git-help
+
+    THESE COMMANDS ARE CREATED WITH THE HELP OF GITHUB API.
 EOT;
 
         $this->info($helpText);
